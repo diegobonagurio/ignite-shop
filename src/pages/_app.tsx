8130b352ae8929fb7bb366cbb1c713a90fe6d853
Diego Bonagurio <diego.bonagurio@gmail.com>
2022-09-23 17:12:19 -0300

    changes

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
+
+import logoImg from "../assets/logo.svg";
+import { Container, Header } from "../styles/pages/app";
+
+import Image from "next/future/image";
+
+export default function App({ Component, pageProps }: AppProps) {
+  globalStyles();
+
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="Logo" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
+}
